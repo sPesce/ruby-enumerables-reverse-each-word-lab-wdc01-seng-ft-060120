da4e2b8b1bd4e17c5fac972003cbd62131231537
stephen pesce <stevepesce879@gmail.com>
2020-05-04 22:42:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word(sentence)
-  p sentence 
+  #sentence array 
   sent_arr = sentence.split(' ')
   
+  #reverse each word in place in array, then reduce back to string 
   reverse = sent_arr.collect{|word| word.reverse}.reduce{|memo, ele|  memo += " #{ele}"}
 end
